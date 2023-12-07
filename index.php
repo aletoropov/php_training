fd822647f945e3fc0bc3cafded0a32e4f68bc067
Alexandr <toropovsite@yandex.ru>
2023-12-08 01:33:59 +0300

    Добавил HTML разметку

--- index.php
+++ index.php
@@ -10,9 +10,26 @@ $ignore_files = ['.', '..', '.git', '.gitignore', 'image.php', '.idea', 'README.
 
 $files = scandir('.');
 
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>PHP-training</title>
+</head>
+<body>   
+ 
+<?php
+
 foreach ($files as $file) {
     if (in_array($file, $ignore_files)) {
         continue;
     }
     echo '<div><a href="'.$file.'">'.$file.'</div>';
 }
+
+?>
+
+</body>
+</html>
