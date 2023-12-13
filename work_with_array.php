6ddb92b48e7a4c3fc204983b19eee584f176b8ca
Alexandr <toropovsite@yandex.ru>
2023-12-13 23:47:43 +0300

    Использование функции для вывода отладочной информации

--- work_with_array.php
+++ work_with_array.php
@@ -39,19 +39,19 @@ for($i = 0; $i < count($arr); $i++) {
  * Удаляем пустые элементы из массива
  */
 $products = ['apple', 'kiwi', '', 'banana', ''];
-var_dump($products);
+debug($products);
 $products = array_diff($products, ['']);
-var_dump($products);
+debug($products);
 
 /**
  * Преобразовываем массив в JSON и обратно
  */
 $cars = ['model' => 'audi', 'year' => 2023, 'speed' => 240];
 $cars_json = json_encode($cars);
-var_dump($cars_json);
+debug($cars_json);
 
 $cars_array = json_decode($cars_json, true);
-var_dump($cars_array);
+debug($cars_array);
 
 /**
  * Объединение массивов
