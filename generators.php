250a2202c2b1d46e7817d1b99a2953ef1092ece4
Alexandr <toropovsite@yandex.ru>
2023-12-21 10:40:31 +0300

    Работа с генераторами в PHP

--- generators.php
+++ generators.php
@@ -2,22 +2,35 @@
 
 /**
  * Работа с генераторами.
+ * При использовании генираторов - в памяти хранится только одно число.
  * 
  * @author Toropov Alexandr
  */
 
- function gen(int $from, int $to) {
-     if ($from < 0 || $to < 0) {
-         return false;
-     }
-     for ($i = $from; $i <= $to; $i++) {
-         yield rand(1, 100);
-     }
- }
+function getGen(int $from, int $to): bool|Generator
+{
+    if ($from < 0 || $to < 0) {
+        return false;
+    }
+    for ($i = $from; $i <= $to; $i++) {
+        yield rand(1, 100);
+    }
+}
 
-foreach (gen(1, 15) as $value) {
+foreach (getGen(1, 15) as $value) {
     $arr[] = $value;
     echo $value. '<br>';
 }
 
 var_dump($arr);
+
+function tenNumbers(): Generator
+{
+    for ($i = 1; $i <= 10; $i++) {
+        yield $i;
+    }
+}
+
+foreach (tenNumbers() as $n) {
+    echo $n;
+}
