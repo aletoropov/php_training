0928ce9cbdf717e8d7633cc7fcf8cfea36d2087b
Alexandr <toropovsite@yandex.ru>
2023-12-25 00:33:53 +0300

    Улучшения внешнего вида примера работы с Captcha

--- captcha/index.php
+++ captcha/index.php
@@ -4,15 +4,8 @@
  *
  * @author Toropov Alexandr <toropovsite@yandex.ru>
  */
-require_once 'captcha.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'captcha.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-   if (Captcha::check($_POST['captcha'])) {
-        echo 'Правильный код!';
-   } else {
-        echo 'Неправильный код!';
-   }
-}
 ?>
 
 <!doctype html>
@@ -22,14 +15,31 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
  <meta name="viewport"
        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
  <meta http-equiv="X-UA-Compatible" content="ie=edge">
- <title>Document</title>
+ <title>Проверка кода Captcha</title>
 </head>
 <body>
   <form method="post">
-    <div>
-     <img src="image.php">
-     <input type="text" name="captcha">
-     <input type="submit" value="Отправить">
+    <div style="max-width: 300px; margin: 10px auto; text-align: center; font-weight: bold;">
+
+        <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            if (Captcha::check($_POST['captcha'])) {
+                echo '<div style="color: green; margin: 5px;">Правильный код!</div>';
+            } else {
+                echo '<div style="color: red; margin: 5px;">Неправильный код!</div>';
+            }
+        }
+        ?>
+
+     <div>
+         <img src="image.php" alt="">
+     </div>
+        <label>
+            <input type="text" name="captcha">
+        </label>
+        <div style="margin: 20px;">
+            <input type="submit" value="Отправить">
+        </div>
     </div>
   </form>
 </body>
