64c24b28f48174a12dcbd6468ba1f67ec75664d0
Alexandr <toropovsite@yandex.ru>
2024-02-11 22:09:30 +0300

    Форма для отправки

--- ajax_send/index.php
+++ ajax_send/index.php
@@ -8,6 +8,25 @@
     <title>Отправка данных с помощью AJAX запроса</title>
 </head>
 <body>
-
+    <form name="send_form" method="post" action="handler.php">
+        <div>
+            <label>
+                <input name="name" type="text">
+            </label>
+        </div>
+        <div>
+            <label>
+                <input name="email" type="email">
+            </label>
+        </div>
+        <div>
+            <label>
+                <textarea name="text"></textarea>
+            </label>
+        </div>
+        <div>
+            <input type="submit" value="Отправить">
+        </div>
+    </form>
 </body>
 </html>
