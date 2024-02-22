5b031272e5922324ed21c118f1b5dfad1dff1805
Alexandr <toropovsite@yandex.ru>
2024-02-23 02:19:10 +0300

    JavaScript обработчик формы и AJAX отправки

--- ajax_send/send.js
+++ ajax_send/send.js
@@ -1,3 +1,25 @@
 jQuery(document).ready(function() {
-  
+  jQuery('form').on('submit', function (e) {
+      e.preventDefault();
+      let form = $(this);
+
+      jQuery.ajax({
+          type: form.attr('method'),
+          url: form.attr('action'),
+          data: form.serialize(),
+          success: function (response) {
+              console.log('success');
+              console.log(response);
+          },
+          error: function (error) {
+              console.log('error');
+              console.log(error);
+          },
+          beforeSend: function () {
+              console.log('before send');
+          }
+      });
+
+      return false;
+  });
 });
